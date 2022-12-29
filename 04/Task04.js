ae98712dc5a2167137bf8e376d3ec3cca4a372a6
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2022-12-29 14:31:49 +0100

    tasks 4/5 completed

--- 04/Task04.js
+++ 04/Task04.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import Shop from '../src/components/Shop';
+import products from './../src/products.json';
+import { Route, useHistory, Redirect } from 'react-router-dom'
+
+
 
 const Task04 = () => {
+    const history = useHistory()
+
+    const handleClick = (e) => {
+        const filters = e.target.value
+        history.push(`/task04/${filters}`)
+    }
     return (
-        <h1>Task04</h1>
+        <>
+            <h1>Task04</h1>
+            <select name="filter" onClick={handleClick}>
+                <option value='price-asc'>Ascending</option>
+                <option value='price-dsc'>Descending</option>
+            </select>
+            <Route path="/task04/price-asc">
+                <Shop items={[...products].sort((a, b) => a.price - b.price)} />
+            </Route>
+            <Route path="/task04/price-dsc">
+                <Shop items={[...products].sort((a, b) => b.price - a.price)} />
+            </Route>
+        </>
     );
 }
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,16 @@
 import React from "react"
+import { NavLink } from 'react-router-dom'
+import Task02 from "../../02/Task02"
+
+
 
 export const Product = ({ product }) => {
     return (
         <ul>
-            <li>Category: {product.category}</li>
-            <li>Name: {product.name}</li>
+            <li><NavLink to={`/task02/product-${product.id}`}>Name: {product.name}</NavLink></li>
             <li>Price: {product.price}</li>
             <li>Description: {product.description}</li>
+            <li>Category: {product.category}</li>
         </ul>
     )
 }
